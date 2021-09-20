c5ca5900799cc82eb67fef88fe7d3240b39307b3
Belal Samy <belalsamy10@gmail.com>
2021-09-21 00:52:20 +0200

    Initial Commit

---
+++ ImageCarousel-Example/CollectionCell.swift
@@ -0,0 +1,17 @@
+//
+//  CollectionCell.swift
+//  ImageCarousel-Example
+//
+//  Created by Belal Samy on 20/09/2021.
+//
+
+import UIKit
+
+class CollectionCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ ImageCarousel-Example/Movie.swift
@@ -0,0 +1,8 @@
+//
+//  Movie.swift
+//  ImageCarousel-Example
+//
+//  Created by Belal Samy on 20/09/2021.
+//
+
+import Foundation
